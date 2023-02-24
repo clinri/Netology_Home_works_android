52e5c8b67c046ce3672f9996a4f4ab98e937b68a
Andrei Borisov <clinri@ya.ru>
2023-02-24 21:20:09 +0300

    Typed code from lecture 14 pushes before Task_#1_RecipientId

--- app/src/main/java/ru/netology/nmedia/auth/AppAuth.kt
+++ app/src/main/java/ru/netology/nmedia/auth/AppAuth.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import androidx.core.content.edit
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import ru.netology.nmedia.api.PostsApi
+import ru.netology.nmedia.dto.PushToken
 import ru.netology.nmedia.model.AuthModel
 
 class AppAuth private constructor(context: Context) {
@@ -21,6 +25,7 @@ class AppAuth private constructor(context: Context) {
         } else {
             _authStateFlow = MutableStateFlow(AuthModel(id, token))
         }
+        sendPushToken()
     }
 
     val authStateFlow = _authStateFlow.asStateFlow()
@@ -31,6 +36,7 @@ class AppAuth private constructor(context: Context) {
         prefs.edit {
             putLong(ID_KEY, id)
             putString(TOKEN_KEY, token)
+            sendPushToken()
         }
     }
 
@@ -39,6 +45,18 @@ class AppAuth private constructor(context: Context) {
         _authStateFlow.value = null
         prefs.edit {
             clear()
+        }
+        sendPushToken()
+    }
+
+    fun sendPushToken(tokenFirebase: String? = null) {
+        CoroutineScope(Dispatchers.Default).launch {
+            val tokenFireBaseNotNull = tokenFirebase ?: FirebaseMessaging.getInstance().token.await()
+            try {
+                PostsApi.service.sendPushToken(PushToken(tokenFireBaseNotNull))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -4,17 +4,14 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import ru.netology.nmedia.R
-import kotlin.random.Random
+import ru.netology.nmedia.auth.AppAuth
 
 
 class FCMService : FirebaseMessagingService() {
-    private val action = "action"
     private val content = "content"
     private val channelId = "remote"
     private val gson = Gson()
@@ -34,44 +31,11 @@ class FCMService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
-
-        message.data[action]?.let {
-           when (Action.valueOf(it)) {
-              Action.LIKE -> handleLike(gson.fromJson(message.data[content], Like::class.java))
-           }
-        }
+        println(message.data["content"])
     }
 
-    override fun onNewToken(token: String) {
-        println(token)
-    }
-
-    private fun handleLike(content: Like) {
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.ic_notification)
-            .setContentTitle(
-                getString(
-                    R.string.notification_user_liked,
-                    content.userName,
-                    content.postAuthor,
-                )
-            )
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
-
-        NotificationManagerCompat.from(this)
-            .notify(Random.nextInt(100_000), notification)
+    override fun onNewToken(tokenFirebase: String) {
+        println("tokenFirebase: $tokenFirebase")
+        AppAuth.getInstance().sendPushToken(tokenFirebase)
     }
 }
-
-enum class Action {
-    LIKE,
-}
-
-data class Like(
-    val userId: Long,
-    val userName: String,
-    val postId: Long,
-    val postAuthor: String,
-)
-
