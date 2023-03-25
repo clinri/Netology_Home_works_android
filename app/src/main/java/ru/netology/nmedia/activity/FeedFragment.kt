2b2ced9e8dec2131044f2e8aa981d5f7412b6425
Andrei Borisov <clinri@ya.ru>
2023-03-25 18:10:45 +0300

    Done task "Paging Refresh, Prepend & Append" in part of implement show Refreshing SwipeRefreshLayout only while REFRESH.

--- app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -138,8 +138,6 @@ class FeedFragment : Fragment() {
         lifecycleScope.launchWhenCreated {
             adapter.loadStateFlow.collectLatest {
                 binding.swiperefresh.isRefreshing = it.refresh is LoadState.Loading
-                        || it.append is LoadState.Loading
-                        || it.prepend is LoadState.Loading
             }
         }
 
